e8cd1efd76919f0a93193210fdbf229abe1fd55a
kbetes <121203524+kbetes@users.noreply.github.com>
2023-01-25 18:22:52 -0500

    update Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -2,18 +2,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
 
 public class Homework17 extends BaseTest {
 
-    @Test
-    public void addSongToPlaylist() throws InterruptedException {
-        login("demo@class.com", "te$t$tudent");
-        Thread.sleep(2000);
-        grabASong();
+    HomePage homepage = new HomePage(driver);
+    LoginPage loginPage = new LoginPage(driver);
 
+    @Test
+   public void addSongToPlaylist()  {
+        loginPage.provideEmail("demo@class.com");
+        loginPage.providePassword("te$t$tudent");
+        loginPage.clickSubmitBtn();
     }
 
-    public void grabASong(){
+
+//        login("demo@class.com", "te$t$tudent");
+//        Thread.sleep(2000);
+//        grabASong();
+//
+//    }
+//
+public void grabASong(){
         WebElement song = driver.findElement(By.xpath("//article[@data-test='song-card']"));
         WebElement playlist = driver.findElement(By.xpath("//section[@id='playlists']//li[5]"));
 
@@ -24,7 +35,7 @@ public class Homework17 extends BaseTest {
                 .perform();
 
         playlist.click();
-//        acts.dragAndDrop(song, playlist).build().perform();
+////        acts.dragAndDrop(song, playlist).build().perform();
 
     }
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,24 +3,30 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class HomePage extends BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
 
-   WebDriver driver;
-  WebDriverWait wait;
 
-    By userAvatarIcon = By.cssSelector("img.avatar");
-     public HomePage (WebDriver givenDriver) {
-         super(givenDriver);
-         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-     }
+    By avatarIcon = By.cssSelector("img.avatar");
+    By song = By.xpath("//article[@data-test='song-card']");
+    By playlist = By.xpath("//section[@id='playlists']//li[5]");
+    By userAvatarIcon;
 
-     public WebElement getUserAvatar(){
-         return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
-     }
+    public HomePage(WebDriver givenDriver) {
+        super(givenDriver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public WebElement getUserAvatar() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
+    }
+
 
 }
